42b265f44415dce2c34d53766d0d67e68532fa03
Ryan B <brockhoffrw@gmail.com>
2019-12-08 11:09:04 +0200

    feat: add app navigation boilerplate.

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import SearchScreen from "./src/screens/SearchScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen
   },
-});
+  {
+    initialRouteName: "Search",
+    defaultNavigationOptions: {
+      title: "Business Search"
+    }
+  }
+);
+
+export default createAppContainer(navigator);
+
+// Navigator doesn't return a React Component. CreateAppContainer is a wrapper that helps navigator return an RC with the navigation we set up above.
